9e4c220ee4aa50960e183266a3de90916c158791
Roberto Freires <rfsinfomanutencao@gmail.com>
2024-06-20 14:15:00 -0300

    Alteração texto alternativo da imagem (Light mode)

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,15 @@ function togglemode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light}
-    img.setAttribute("src", "./assets/avatar.light.png")
+    img.setAttribute("src", "./assets/avatar.light.png") 
   } else {
     //se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
+  if (html.classList.contains("light")) {
+    img.alt=("Foto de Roberto Freires, sem camiseta, na academia")
+  } else {
+    img.alt=("Foto de Roberto Freires sorrindo, usando óculos de sol, camiseta preta, barba, dentro do carro.")
+  }
+    
 }
